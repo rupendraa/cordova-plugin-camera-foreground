5f4165a5082464704e8ddebff79490b1f7495f2f
rupendraa <39113656+rupendraa@users.noreply.github.com>
2018-08-06 15:56:40 +0530

    Update ExifHelper.java

--- src/android/ExifHelper.java
+++ src/android/ExifHelper.java
@@ -16,7 +16,7 @@
        specific language governing permissions and limitations
        under the License.
 */
-package org.apache.cordova.camera;
+package co.mwater.foregroundcameraplugin;
 
 import java.io.IOException;
 
